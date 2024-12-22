44972d2ecc4987cebaf8feb558b5e8c026d28014
Nikita Baklanov <nikitabaklanov859@gmail.com>
2024-12-23 01:20:00 +0500

    add recursion

---
+++ Recursion/Recursion.cpp
@@ -0,0 +1,32 @@
+﻿#include <math.h>
+#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+static double f(double x, int n) {
+    return pow(-1, n) * (pow((1 + x), 2 * n) / n);
+}
+
+static double S(double x, int n) {
+    if (n == 0) return 0;
+    if (n == 1) return f(x,1);
+    else {
+        return  f(x,n) + S(x, n - 1);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    double x;
+    cout << "Введите любой x: ";
+    cin >> x;
+    cout << "Введите целый n > 0 (в случае введения некорректного n будет применен n = 1): ";
+    cin >> n;
+    n = n > 0 ? n : 1;
+    cout << "Результатом вычислений будет: " << S(x, n) << endl;
+    system("pause");
+    return 0;
+}
